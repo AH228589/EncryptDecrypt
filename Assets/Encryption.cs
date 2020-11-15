c4656f2402dba1b2d62e9ff9f53bea17a1b01620
Aleksandar Hristov <AH228589@falmouth.ac.uk>
2020-11-15 15:09:45 +0000

    Cybersecurity Initial Commit

---
+++ Assets/Encryption.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public static class Encryption
+{
+    public static string EncryptDecrypt(string inputText, int key)
+    {
+        StringBuilder outSB = new StringBuilder(inputText.Length);
+        for (int i = 0; i < inputText.Length; i++)
+        {
+            char ch = (char)(inputText[i] ^ key);
+            outSB.Append(ch);
+        }
+        return outSB.ToString();
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Encrypted text : " + Encryption.EncryptDecrypt("TESTING TESTING", 420));
+
+        //The string used here is the one we get logged on the line above. - "TESTING TESTING"
+        Debug.Log("Decrypted text : " + Encryption.EncryptDecrypt("ǰǡǷǰǭǪǣƄǰǡǷǰǭǪǣ", 420));
+    }
+}
